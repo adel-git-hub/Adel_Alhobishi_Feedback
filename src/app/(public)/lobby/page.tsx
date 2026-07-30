3ee5261233dea4f926a7e7db6bf59a797bdd6a53
Adel Alhobaishi GitHub <adel.alhobaishi.github@gmail.com>
2026-07-30 19:15:01 +0300

    Delete src directory

--- src/app/(public)/lobby/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getPublicBranches } from "@/app/actions/public.actions";
-import PublicHomeClient from "@/app/(public)/PublicHomeClient";
-
-export default async function LobbyPage() {
-  const branches = await getPublicBranches();
-  return <PublicHomeClient initialBranches={branches} accessType="LOBBY_CUSTOMER" />;
-}
